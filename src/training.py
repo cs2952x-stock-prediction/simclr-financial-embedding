b7590364e3c44cb2aec56ef18c046cdf37719ab5
Ian Gordon <ianbgordon@gmail.com>
2024-10-31 12:38:40 -0400

    large update to training pipeline --- add wandb, update hyperparameters

--- src/training.py
+++ src/training.py
@@ -1,25 +1,57 @@
+import os
+
+import numpy as np
 import pandas as pd
 import torch
+from dotenv import load_dotenv
 from torch.optim import Adam
 from torch.utils.data import DataLoader
+from torchsummary import summary
 from tqdm import tqdm
 
+import wandb
 from datasets import TimeSeriesDataset
 from evaluation import nt_xent_loss
 from models import SimpleLstmEncoder
 from transformations import mask_with_added_gaussian
 
 # Configurations
-embedding_dim = 64  # Dimension of embeddings produced by the model
-hidden_dim = 128  # Hidden LSTM dimension
-output_dim = 64  # Output projection dimension (for SimCLR projection head)
-batch_size = 32  # Number of samples per batch
-temperature = 0.5  # Temperature parameter for NT-Xent loss
-n_epochs = 100  # Number of epochs
 
-segment_length = 14  # on daily entries, this is two weeks
+hidden_size = 128  # Dimension of lstm hidden cell state
+proj_size = 0  # Dimension of lstm output
+output_size = 32  # Output projection dimension (for SimCLR projection head)
+
+learning_rate = 1e-6  # Learning rate for the optimizer
+batch_size = 128  # Number of samples per batch
+n_epochs = 100  # Number of epochs
+temperature = 0.5  # Temperature parameter for NT-Xent loss
 
 data_file = "data/raw/kaggle_sp500/sp500_stocks.csv"
+segment_length = 30  # on daily entries, this is one month
+segment_step = 10  # the distance between the start of each segment
+
+load_dotenv()
+os.environ["WANDB_API_KEY"] = os.getenv("WANDB_API_KEY")
+
+wandb.init(
+    project="simcl-stock-embedding",
+    name="simple-lstm-encoder",
+    config={
+        # model config
+        "hidden_size": hidden_size,
+        "proj_size": proj_size,
+        "output_size": output_size,
+        # training config
+        "learning_rate": learning_rate,
+        "batch_size": batch_size,
+        "n_epochs": n_epochs,
+        "temperature": temperature,
+        # data source+structure config
+        "data_file": data_file,
+        "segment_length": segment_length,
+        "segment_step": segment_step,
+    },
+)
 
 
 def train_one_epoch(model, data_loader, optimizer, temperature):
@@ -29,7 +61,7 @@ def train_one_epoch(model, data_loader, optimizer, temperature):
     pbar = tqdm(data_loader)
     for batch in pbar:
         # Apply two augmentations to the batch
-        x_i = mask_with_added_gaussian(batch.numpy(), mask_prob=0.1, std_multiplier=0.1)
+        x_i = mask_with_added_gaussian(batch.numpy(), mask_prob=1.0, std_multiplier=0.2)
         x_j = batch.numpy()  # NOTE: currently just the identity function
 
         # Convert augmented data to tensors and move to device
@@ -44,7 +76,7 @@ def train_one_epoch(model, data_loader, optimizer, temperature):
         z = torch.stack([z_i, z_j], dim=0)
         loss = nt_xent_loss(z, temperature)
 
-        pbar.set_postfix({"Loss": f"{loss.item():.4f}"})
+        pbar.set_postfix({"Batch Loss": f"{loss.item():.4f}"})
 
         # Backpropagation
         optimizer.zero_grad()
@@ -52,6 +84,7 @@ def train_one_epoch(model, data_loader, optimizer, temperature):
         optimizer.step()
 
         total_loss += loss.item()
+        wandb.log({"Batch Loss": loss.item()})
 
     avg_loss = total_loss / len(data_loader)
     return avg_loss
@@ -61,7 +94,6 @@ if __name__ == "__main__":
     # Load data
     print(f"Loading data from {data_file}")
     df = pd.read_csv(data_file)
-    print(f"Loaded data with shape {df.shape}")
     print(df.head())
 
     # Prepare data for training
@@ -74,7 +106,7 @@ if __name__ == "__main__":
     df["time"] = (datetime - datetime.min()).dt.total_seconds()
 
     # Separate series by symbol
-    print("Separating series by symbol...")
+    print("Separating series by symbol, interpolating, removing nan values...")
     series = []
     symbols = df["symbol"].unique()
     for symbol in tqdm(symbols):
@@ -87,21 +119,33 @@ if __name__ == "__main__":
         series.append(symbol_series.values)
 
     print("Creating dataset and data loader...")
-    dataset = TimeSeriesDataset(series, segment_length)
+    dataset = TimeSeriesDataset(
+        series,
+        segment_length,
+        step=segment_step,
+    )
     data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
     # Initialize model and optimizer
+    print("Initializing model and optimizer...")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = SimpleLstmEncoder(
-        n_features=series[0].shape[-1],
-        embedding_dim=embedding_dim,
-        hidden_dim=hidden_dim,
-        output_dim=output_dim,
+        input_size=series[0].shape[-1],
+        hidden_size=hidden_size,
+        output_size=output_size,
+        proj_size=proj_size,
     ).to(device)
-    optimizer = Adam(model.parameters(), lr=1e-3)
+    optimizer = Adam(model.parameters(), lr=learning_rate)
+    summary(
+        model,
+        input_data=(segment_length, series[0].shape[-1]),
+    )
 
     # Training loop
     print("Starting training loop...")
     for epoch in range(n_epochs):
         avg_loss = train_one_epoch(model, data_loader, optimizer, temperature)
         print(f"Epoch [{epoch+1}/{n_epochs}], Loss: {avg_loss:.4f}")
+        wandb.log({"Epoch": epoch, "Epoch Loss": avg_loss})
+
+    wandb.finish()
