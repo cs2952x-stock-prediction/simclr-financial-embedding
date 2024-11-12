aed536e451ed178f4b416fdc03ebe7b9d5a6d7d9
Ian Gordon <ianbgordon@gmail.com>
2024-11-12 16:41:54 -0500

    add comments to activate.sh

--- activate.sh
+++ activate.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# Check if the 'env' directory exists
+# Check if the 'env' directory exists.
+
+# If it does not exist, create a virtual environment .
+# Then activate it and install the requirements.
 if [ ! -d "venv" ]; then
   echo "Creating virtual environment..."
   python3 -m venv venv
 	source venv/bin/activate
 	pip install -r requirements.txt
+
+# If it exists, activate the virtual environment.
 else
 	source venv/bin/activate
 fi
