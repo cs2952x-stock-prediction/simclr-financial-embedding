6353825615fbcec45f86a9898dee21280285fe07
Ian Gordon <ianbgordon@gmail.com>
2024-11-29 12:53:03 -0500

    move transform and features code to their own util directory + unify epsilon for log transform

--- src/data/util/transform.py
+++ src/data/util/transform.py
@@ -7,13 +7,7 @@ from sklearn.preprocessing import StandardScaler
 
 ################################################# GLOBAL CONSTANTS #################################
 
-# integer-valued columns that can be log-transformed
-INT_LOG_COLUMNS = ["volume"]
-INT_EPS = 1  # quantity to add to integer columns before log-transforming
-
-# float-valued columns that can be log-transformed
-FLOAT_LOG_COLUMNS = ["close", "high", "low", "open", "adj_close"]
-FLOAT_EPS = 0.01  # quadrity to add to float columns before log-transforming
+EPS = 0.01  # quantity to add before taking the log
 
 ################################################ FUNCTIONS #########################################
 
@@ -56,12 +50,7 @@ class DataTransformer:
                     f"Column '{col}' contains negative values; log transform is invalid."
                 )
 
-            if col in INT_LOG_COLUMNS:
-                df.loc[:, col] = np.log(df[col] + INT_EPS)
-            elif col in FLOAT_LOG_COLUMNS:
-                df.loc[:, col] = np.log(df[col] + FLOAT_EPS)
-            else:
-                raise ValueError(f"Unexpected column passed to log transform: '{col}'.")
+            df.loc[:, col] = np.log(df[col] + EPS)
 
         return df
 
@@ -76,12 +65,7 @@ class DataTransformer:
             pd.DataFrame: The transformed DataFrame.
         """
         for col in intersection(self.config.get("log_features", []), df.columns):
-            if col in INT_LOG_COLUMNS:
-                df.loc[:, col] = np.exp(df[col]) - INT_EPS
-            elif col in FLOAT_LOG_COLUMNS:
-                df.loc[:, col] = np.exp(df[col]) - FLOAT_EPS
-            else:
-                raise ValueError(f"Unexpected column passed to log transform: '{col}'.")
+            df.loc[:, col] = np.exp(df[col]) - EPS
 
         return df
 
@@ -206,6 +190,24 @@ class DataTransformer:
         scale_cols = intersection(self.config.get("scale_features", []), df.columns)
         self.scaler.fit(df[scale_cols])
 
+    def partial_fit(self, df: pd.DataFrame, prev_row: Optional[pd.Series] = None):
+        """
+        Update the transformer with new data, learning transformation parameters.
+
+        Args:
+            df (pd.DataFrame): The DataFrame to fit.
+            prev_row (pd.Series): The previous row to difference against.
+
+        Returns:
+            None
+        """
+        df = df.copy()
+        df = self.log_transform(df)
+        df = self.diff_transform(df, prev_row)
+
+        scale_cols = intersection(self.config.get("scale_features", []), df.columns)
+        self.scaler.partial_fit(df[scale_cols])
+
     def fit_transform(self, df: pd.DataFrame, prev_row: Optional[pd.Series] = None):
         """
         Fit the transformer to a DataFrame and apply transformations.
@@ -243,138 +245,3 @@ class DataTransformer:
         """
         with open(filepath, "rb") as f:
             return pickle.load(f)
-
-
-def add_cyclic_day_feature(df: pd.DataFrame, inplace=True):
-    """
-    Add cyclic day features to a DataFrame.
-
-    Args:
-        df (pd.DataFrame): The DataFrame to add features to.
-        inplace (bool): Whether to add the features in place.
-
-    Returns:
-        pd.DataFrame: The DataFrame with added features
-    """
-    if not inplace:
-        df = df.copy()
-
-    timestamps = df["timestamp"].dt
-    fraction_of_day = (
-        timestamps.hour + timestamps.minute / 60 + timestamps.second / 3600
-    ) / 24
-    df["day_sin"] = np.sin(2 * np.pi * fraction_of_day)
-    df["day_cos"] = np.cos(2 * np.pi * fraction_of_day)
-
-    return df
-
-
-def add_cyclic_week_feature(df: pd.DataFrame, inplace=True):
-    """
-    Add cyclic week features to a DataFrame.
-
-    Args:
-        df (pd.DataFrame): The DataFrame to add features to.
-        inplace (bool): Whether to add the features in place.
-
-    Returns:
-        pd.DataFrame: The DataFrame with added features
-    """
-    if not inplace:
-        df = df.copy()
-
-    timestamps = df["timestamp"].dt
-    fraction_of_day = (
-        timestamps.hour + timestamps.minute / 60 + timestamps.second / 3600
-    ) / 24
-    fraction_of_week = (timestamps.dayofweek + fraction_of_day) / 7
-
-    df["week_sin"] = np.sin(2 * np.pi * fraction_of_week)
-    df["week_cos"] = np.cos(2 * np.pi * fraction_of_week)
-
-    return df
-
-
-def add_cyclic_month_feature(df: pd.DataFrame, inplace=True):
-    """
-    Add cyclic month of the year features to a DataFrame.
-
-    Args:
-        df (pd.DataFrame): The DataFrame to add features to.
-        inplace (bool): Whether to add the features in place.
-
-    Returns:
-        pd.DataFrame: The DataFrame with added features
-    """
-    if not inplace:
-        df = df.copy()
-
-    timestamps = df["timestamp"].dt
-    days_in_month = timestamps.dt.days_in_month
-    fraction_of_day = (
-        timestamps.hour + timestamps.minute / 60 + timestamps.second / 3600
-    ) / 24
-    fraction_of_month = (timestamps.dt.day + fraction_of_day) / days_in_month
-    df["month_sin"] = np.sin(2 * np.pi * fraction_of_month)
-    df["month_cos"] = np.cos(2 * np.pi * fraction_of_month)
-
-    return df
-
-
-def add_cyclic_quarter_feature(df: pd.DataFrame, inplace=True):
-    """
-    Add cyclic quarter of the year features to a DataFrame.
-
-    Args:
-        df (pd.DataFrame): The DataFrame to add features to.
-        inplace (bool): Whether to add the features in place.
-
-    Returns:
-        pd.DataFrame: The DataFrame with added features
-    """
-    if not inplace:
-        df = df.copy()
-
-    timestamps = df["timestamp"].dt
-    period = timestamps.dt.to_period("Q")
-    days_in_quarter = period.apply(
-        lambda x: x.end_time.dayofyear - x.start_time.dayofyear
-    )
-    day_of_quarter = timestamps.dt.dayofyear - period.apply(
-        lambda x: x.start_time.dayofyear
-    )
-    fraction_of_day = (
-        timestamps.hour + timestamps.minute / 60 + timestamps.second / 3600
-    )
-    fraction_of_quarter = (day_of_quarter + fraction_of_day) / days_in_quarter
-    df["quarter_sin"] = np.sin(2 * np.pi * fraction_of_quarter)
-    df["quarter_cos"] = np.cos(2 * np.pi * fraction_of_quarter)
-
-    return df
-
-
-def add_cyclic_year_feature(df: pd.DataFrame, inplace=True):
-    """
-    Add cyclic year features to a DataFrame.
-
-    Args:
-        df (pd.DataFrame): The DataFrame to add features to.
-        inplace (bool): Whether to add the features in place.
-
-    Returns:
-        pd.DataFrame: The DataFrame with added features
-    """
-    if not inplace:
-        df = df.copy()
-
-    timestamps = df["timestamp"].dt
-    days_in_year = timestamps.dt.to_period("Y").apply(lambda x: x.end_time.dayofyear)
-    day_of_year = timestamps.dt.dayofyear
-    fraction_of_day = (
-        timestamps.hour + timestamps.minute / 60 + timestamps.second / 3600
-    )
-    fraction_of_year = (day_of_year + fraction_of_day) / days_in_year
-    df["year_sin"] = np.sin(2 * np.pi * fraction_of_year)
-    df["year_cos"] = np.cos(2 * np.pi * fraction_of_year)
-
-    return df
